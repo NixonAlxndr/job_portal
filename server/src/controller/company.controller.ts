da16165316bb553b64dbf891c1fcc620a76937b6
NIXON ALEXANDER SIMON SURYA KUDUS <nixon.kudus@binus.edu>
2025-10-03 10:54:34 +0700

    add company route and controller

--- server/src/controller/company.controller.ts
+++ server/src/controller/company.controller.ts
@@ -9,10 +9,39 @@ type CompanyType = {
     userId: string
 }
 
-export const createCompany = async (req : Request, res: Response) => {
+type GetCompanyType = {
+    userId: string
+}
+
+export const getCompany = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params as GetCompanyType
+
+        if (!userId) {
+            res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        const company = await prisma.company.findMany({
+            where: { userId }
+        })
+
+
+        res.status(200).json({
+            company
+        })
+    } catch (err) {
+        res.status(500).json({
+            messsage: "Internal server error"
+        })
+    }
+}
+
+export const createCompany = async (req: Request, res: Response) => {
     try {
         const { name, place, latitude, longitude, userId } = req.body as unknown as CompanyType;
-        
+
         const company = await prisma.company.create({
             data: {
                 name, place, latitude, longitude, userId
@@ -23,8 +52,51 @@ export const createCompany = async (req : Request, res: Response) => {
             message: "Company Created",
             company
         })
-    }catch(err) {
+    } catch (err) {
         res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+export const updateCompany = async (req: Request, res: Response) => {
+    try {
+        const { name, place, latitude, longitude} = req.body as unknown as CompanyType;
+        const { userId } = req.params as GetCompanyType;
+
+        const company = await prisma.company.update({
+            where: { userId },
+            data: {
+                name, place, latitude, longitude
+            }
+        })
+
+        res.status(200).json({
+            message: "Company updated",
+            company
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+export const deleteCompany = async (req: Request, res: Response) => {
+    try {
+        const { companyId } = req.params as { companyId: string }
+
+        const company = await prisma.company.delete({
+            where: { id: companyId },
+        })
+
+        res.status(200).json({
+            message: "Company updated",
+            company
+        })
+    } catch (err) {
+        res.status(500).json({
+            error: err,
             message: "Internal server error"
         })
     }
--- server/src/routes/company.route.ts
+++ server/src/routes/company.route.ts
@@ -1,8 +1,11 @@
-import { createCompany } from "@/controller/company.controller.js"
+import { createCompany, deleteCompany, getCompany, updateCompany } from "@/controller/company.controller.js"
 import express from "express"
 
 const router = express.Router()
 
 router.post("/", createCompany)
+router.get("/:userId", getCompany)
+router.patch("/:userId", updateCompany)
+router.delete("/:companyId", deleteCompany)
 
 export default router
